28c718b975d3d6f247d507c4851e3332de5d87a1
shuaibuaa <782385373@qq.com>
2016-01-24 01:03:43 +0800

    20160123

---
+++ src/src/DrawPlot.java
@@ -0,0 +1,203 @@
+package src;
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2004, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * in the United States and other countries.]
+ *
+ * -------------------
+ * LineChartDemo6.java
+ * -------------------
+ * (C) Copyright 2004, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * $Id: LineChartDemo6.java,v 1.5 2004/04/26 19:11:55 taqua Exp $
+ *
+ * Changes
+ * -------
+ * 27-Jan-2004 : Version 1 (DG);
+ * 
+ */
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.*;
+//import org.jfree.ui.Spacer;
+import org.jfree.chart.*;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+/**
+ * A simple demonstration application showing how to create a line chart using data from an
+ * {@link XYDataset}.
+ *
+ */
+public class DrawPlot extends ApplicationFrame {
+
+    /**
+     * Creates a new demo.
+     *
+     * @param title  the frame title.
+     */
+    public DrawPlot(final String title,ArrayList<Double> F2,ArrayList<Double> F2_up) {
+
+        super(title);
+
+        final XYDataset dataset = createDataset(F2,F2_up);
+        final JFreeChart chart = createChart(dataset);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(chartPanel);
+
+    }
+    
+    /**
+     * Creates a sample dataset.
+     * 
+     * @return a sample dataset.
+     */
+    private XYDataset createDataset(ArrayList<Double> F2,ArrayList<Double> F2_up) {
+        
+        final XYSeries series1 = new XYSeries("F");
+        for(int i=0;i<F2.size();i++)
+        	series1.add(i, F2.get(i));
+        
+        final XYSeries series2 = new XYSeries("F_low");
+        for(int i=0;i<F2_up.size();i++)
+        	series2.add(i, F2_up.get(i));
+        
+        
+
+ 
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+        
+        return dataset;
+        
+    }
+    
+    /**
+     * Creates a chart.
+     * 
+     * @param dataset  the data for the chart.
+     * 
+     * @return a chart.
+     */
+    private JFreeChart createChart(final XYDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "F2 and F2_up",      // chart title
+            "Top ith Doc",                      // x axis label
+            "Realted Scores",                      // y axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL,
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+        chart.setBackgroundPaint(Color.white);
+
+//        final StandardLegend legend = (StandardLegend) chart.getLegend();
+  //      legend.setDisplaySeriesShapes(true);
+        
+        // get a reference to the plot for further customisation...
+        final NumberAxis domainAxis = new NumberAxis("X-Axis");
+        domainAxis.setRange(0.0,101.00);
+        domainAxis.setTickUnit(new NumberTickUnit(1.0));
+        final NumberAxis rangeAxis = new NumberAxis("Y-Axis");
+        rangeAxis.setRange(0.00,1.20);
+        rangeAxis.setTickUnit(new NumberTickUnit(0.01));
+
+        /*final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(0.0, 1.0, 0.0, 30.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(1, false);
+        plot.setRenderer(renderer);*/
+
+        // change the auto tick unit selection to integer units only...
+        //final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        //rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        // OPTIONAL CUSTOMISATION COMPLETED.
+                
+        return chart;
+        
+    }
+
+    // ****************************************************************************
+    // * JFREECHART DEVELOPER GUIDE                                               *
+    // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+    // * to purchase from Object Refinery Limited:                                *
+    // *                                                                          *
+    // * http://www.object-refinery.com/jfreechart/guide.html                     *
+    // *                                                                          *
+    // * Sales are used to provide funding for the JFreeChart project - please    * 
+    // * support us so that we can continue developing free software.             *
+    // ****************************************************************************
+    
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void draw(ArrayList<Double> F2,ArrayList<Double> F2_up){
+        
+        final DrawPlot demo = new DrawPlot("Line Chart Demo 6",F2,F2_up);
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+    }
+    public static void main(final String[] args) {
+    	//double[] prob=new double[]{0.7756, 0.7507, 0.7173, 0.6886, 0.671, 0.6606, 0.6025, 0.5809, 0.5779, 0.548, 0.5451, 0.5393, 0.5222, 0.5222, 0.5193, 0.5152, 0.5152, 0.5082, 0.4972, 0.4957, 0.4939, 0.4888, 0.4878, 0.4734, 0.4567, 0.4543, 0.453, 0.4477, 0.4428, 0.4297,1.0};
+        
+       // final JfreeChartTest demo = new JfreeChartTest("Line Chart Demo 6");
+        //demo.pack();
+       // RefineryUtilities.centerFrameOnScreen(demo);
+        //demo.setVisible(true);
+
+    }
+
+}
---
+++ src/src/RandomGreedy.java
@@ -0,0 +1,550 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class RandomGreedy {
+	
+	static Connection conn = null;
+	static String day;
+	static double alpha = 0.15;
+	static double score;
+	static double lastscore;
+	static double[][] pdv;
+	static double FY;
+	static double GYY;
+	static double FV;
+	static ArrayList<String> V1   = new ArrayList<String>();
+	static ArrayList<String> V2   = new ArrayList<String>();
+	static ArrayList<String> S1r  = new ArrayList<String>();
+	static ArrayList<String> S2r  = new ArrayList<String>();
+	static ArrayList<String> S1rt = new ArrayList<String>();
+	static ArrayList<String> S2rt = new ArrayList<String>();
+	static ArrayList<String> random = new ArrayList<String>();
+	static HashMap<String, Integer> u2i = new HashMap<String, Integer>();
+	static HashMap<String, Integer> k2i = new HashMap<String, Integer>();
+	static HashMap<String, String> userLocMap = new HashMap<String, String>();
+	static HashMap<String, String> locMap = new HashMap<String, String>();
+	static HashMap<String, Integer> locNumMap = new HashMap<String, Integer>();
+	static HashMap<String, Integer> locConnNumMap = new HashMap<String, Integer>();
+
+	static ArrayList<Double> F2   = new ArrayList<Double>();
+	static ArrayList<Double> F2_Up= new ArrayList<Double>(); 
+	static ArrayList<Double> F   = new ArrayList<Double>();
+	static ArrayList<Double> F_Low= new ArrayList<Double>();
+
+	public static void randomGreedy(Date date) {
+		init(date);
+		greedy();
+	}
+	
+	private static void init(Date date) {
+		//init day
+		initDate(date);
+		//init V1,V2,userlocMap,pdv
+		initV();
+		//init locNumMap
+		initLocNumMap();
+	}
+	
+	private static void initDate(Date date) {
+		int y, m, d;
+		String yy, mm, dd;
+		y = date.getYear()+1900;
+		m = date.getMonth()+1;
+		d = date.getDate();
+		yy = y + "";
+		if(m<10)
+			mm = "0" + m;
+		else
+			mm = "" + m;
+		if(d<10)
+			dd = "0" + d;
+		else
+			dd = "" + d;
+		day = yy+"-"+mm+"-"+dd; // to obtain this "day", be useful when visiting databases
+	}
+	
+	/*
+	 * need to initial your Database's "user" and "password"
+	 */
+	private static void initDB(){
+		String url = "jdbc:mysql://localhost:3306/gasdm?user=root&password=&useUnicode=true&characterEncoding=UTF8";
+		try {  
+			Class.forName("com.mysql.jdbc.Driver");  
+			conn = DriverManager.getConnection(url);
+		} catch (Exception e) {  
+			e.printStackTrace();  
+		}
+	}
+	
+	// initial the ground set V, including all users and keywords
+	private static void initV() {
+		Statement stmt;
+		ResultSet urs, krs, prs;
+		String user, keyword, loc;
+		double pvalue;
+		int index = 0;
+		try {
+			stmt = conn.createStatement();
+			//init V1, userLocMap, u2i
+			String sql = "select distinct u_id, location from pdven where day = '" + day + "';";	
+			urs = stmt.executeQuery(sql);
+			V1.clear();u2i.clear();userLocMap.clear();
+			while(urs.next()){
+				user = urs.getString("u_id");
+				loc = urs.getString("location");
+				V1.add(user);
+				u2i.put(user, index++); // revert user to index in the array[][] "pdv"
+				userLocMap.put(user, loc);
+			}
+			// init V2, k2i
+			sql = "select distinct keyword from pdven where day = '" + day + "';";
+			index = 0;
+			krs = stmt.executeQuery(sql);
+			V2.clear();k2i.clear();
+			while(krs.next()){
+				keyword = krs.getString("keyword");
+				V2.add(keyword);
+				k2i.put(keyword, index++); // revert keyword to index in the array[][] "pdv"
+			}
+			// init the array[][] "pdv", pdv[keyword][user]=the pvalue of the pair{"keyword", "user"} of this day
+			pdv = new double[V2.size()][V1.size()];
+			for(int i=0; i<V2.size(); i++)
+				for(int j=0; j<V1.size(); j++)
+					pdv[i][j]=1;
+			sql = "select u_id, keyword, pvalue from pdven where day = '"+ day +"';"; 
+			prs = stmt.executeQuery(sql);
+			while(prs.next()){
+				user = prs.getString("u_id");
+				keyword = prs.getString("keyword");
+				pvalue = prs.getDouble("pvalue");
+				pdv[k2i.get(keyword)][u2i.get(user)] = pvalue;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// initial locMap<String, String>, <province, its neighbor provinces>
+	private static void initLocMap() {//shan1xi and shan3xi are different province
+		locMap.put("beijing","hebei|tianjin");
+		locMap.put("tianjin","hebei|beijing");
+		locMap.put("hebei","beijing|tianjin|shan1xi|henan|shandong|liaoning|neimenggu");
+		locMap.put("henan","hebei|shandong|shan1xi|shan3xi|hubei|anhui|jiangsu");
+		locMap.put("shan1xi","henan|hebei|shan3xi|neimenggu");
+		locMap.put("shandong","hebei|henan|anhui|jiangsu");
+		locMap.put("neimenggu","gansu|ningxia|shan3xi|shan1xi|hebei|liaoning|jilin|heilongjiang");
+		locMap.put("liaoning","hebei|neimenggu|jilin");
+		locMap.put("jilin","neimenggu|liaoning|heilongjiang");
+		locMap.put("heilongjiang","jilin|neimenggu");
+		locMap.put("shan3xi","gansu|sichuan|chongqing|hubei|henan|shan1xi|neimenggu");
+		locMap.put("hubei","shan3xi|henan|anhui|chongqing|hunan|jiangxi");
+		locMap.put("hunan","guizhou|guangdong|hubei|chongqing|jiangxi|guangxi");
+		locMap.put("anhui","jiangsu|henan|hubei|jiangxi|zhejiang");
+		locMap.put("jiangsu","shandong|anhui|zhejiang|shanghai");
+		locMap.put("gansu","ningxia|shan3xi|neimenggu|xinjiang|qinghai|sichuan");
+		locMap.put("ningxia","gansu|neimenggu|shan3xi");
+		locMap.put("sichuan","guizhou|yunnan|chongqing|shan3xi|qinghai|xizang|gansu");
+		locMap.put("chongqing","hubei|shan3xi|sichuan|guizhou|hunan");
+		locMap.put("jiangxi","hubei|hunan|guangdong|fujian|zhejiang|anhui");
+		locMap.put("zhejiang","jiangxi|fujian|anhui|jiangsu|shanghai");
+		locMap.put("shanghai","jiangsu|zhejiang");
+		locMap.put("xinjiang","xizang|qinghai|gansu");
+		locMap.put("xizang","qinghai|sichuan|xinjiang|yunnan");
+		locMap.put("yunnan","xizang|guangxi|sichuan|guizhou");
+		locMap.put("guangxi","guangdong|hunan|guizhou|yunnan");
+		locMap.put("guangdong","guangxi|hunan|jiangxi|fujian|hainan");
+		locMap.put("qinghai","gansu|sichuan|xinjiang|xizang");
+		locMap.put("fujian","jiangxi|guangdong|taiwan|zhejiang");
+		locMap.put("guizhou","yunnan|sichuan|chongqing|hunan|guangxi");
+		locMap.put("hainan","guangdong");
+		locMap.put("xianggang","guangdong|aomen");
+		locMap.put("aomen","guangdong|xianggang");
+		locMap.put("taiwan","fujian");
+	}
+	
+	//initial locNumMap<String, Integer>, <province, the user number of this province)>
+	private static void initLocNumMap() {
+		Iterator iter = userLocMap.entrySet().iterator();
+		locNumMap.clear();
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			Object loc = entry.getValue();
+			if (!locNumMap.containsKey(loc)) {
+				locNumMap.put(loc.toString(), 1);
+			}
+			else {
+				locNumMap.put(loc.toString(), locNumMap.get(loc.toString())+1);
+			}
+		}
+	}
+	
+	//greedy algorithm
+	private static void greedy() {
+		int r = 1;
+		FV = f2(V1,V2); // will be use in function G, to reduce the complexity
+		random.addAll(V1);
+		random.addAll(V2);
+		S2r.add("wumai");
+		random.remove("wumai");
+		int index = 0;
+		do {
+			System.out.println("r = "+r);
+			r = r + 1;
+			S1rt.clear();S1rt.addAll(S1r);
+			S2rt.clear();S2rt.addAll(S2r);
+			randomPermutation();
+			
+			FY = f2(S1rt, S2rt);// will be use in function M and G
+			GYY = G2(S1rt, S2rt, S1rt, S2rt);// will be use in function G, (S1t for S1(t-1) and S2t for S2(t-1), refers to the result sets of last iteration)
+			
+//			System.out.println("original "+r+" score: "+ (f1(S1rt, S2rt)- f2(S1rt, S2rt)+penalty(S1rt)));
+//			System.out.println("begin " +r+" lower bound score: "+ (f1(S1rt, S2rt)- M(S1rt, S2rt, S1rt, S2rt)+penalty(S1rt)));
+			
+			for (int i=0; i<random.size(); i++) {
+				ArrayList<String> t = new ArrayList<String>();
+				String e = random.get(i);
+				// there is a little different from the theory to reduce the complexity, cause (add keyword to user set)=0, (add user to keyword set)=0
+				if (isUser(e)) {// there are only numbers in e, indicate that it's a user
+					double x = f(S1r, S2r, S1rt, S2rt);
+					t.clear();t.addAll(S1r);
+					t.add(e);
+					double y = f(t, S2r, S1rt, S2rt);
+					x = y - x;
+					if (x > 0){
+						S1r.add(e);
+						score = y;
+						System.out.println(index++ + ": " +e);
+//						System.out.println("original score: "+ (f1(S1r, S2r)- f2(S1r, S2r) + penalty(S1r)));
+//						System.out.println("lower bound score: "+ score + "\t"+((f1(S1r, S2r)- f2(S1r, S2r) + penalty(S1r))>=score?true:false));
+//						System.out.println("original F2: "+ f2(S1r, S2r));
+//						System.out.println("upper bound MF2: "+ M(S1r, S2r, S1rt, S2rt) + "\t"+(M(S1r, S2r, S1rt, S2rt)>=f2(S1r, S2r)?true:false));
+//						System.out.println();
+						F2.add(f2(S1r, S2r));                //F2 score
+						F2_Up.add(M(S1r, S2r, S1rt, S2rt));  //F2_upper bound
+						F.add((f1(S1r, S2r)- f2(S1r, S2r) + penalty(S1r)));                //F score
+						F_Low.add((f1(S1r, S2r)- M(S1r, S2r, S1rt, S2rt) + penalty(S1r))); //F_lower bound
+					}
+				}
+				else {// e is a keyword
+					double x = f(S1r, S2r, S1rt, S2rt);
+					t.clear();t.addAll(S2r);
+					t.add(e);
+					double y = f(S1r, t, S1rt, S2rt);
+					x = y - x;
+					if (x > 0){
+						S2r.add(e);
+						score = y;
+						System.out.println(index++ + ": " +e);
+//						System.out.println("original score: "+ (f1(S1r, S2r)- f2(S1r, S2r) + penalty(S1r)));
+//						System.out.println("lower bound score: "+ score + "\t"+((f1(S1r, S2r)- f2(S1r, S2r) + penalty(S1r))>=score?true:false));
+//						System.out.println("original F2: "+ f2(S1r, S2r));
+//						System.out.println("upper bound MF2: "+ M(S1r, S2r, S1rt, S2rt) + "\t"+(M(S1r, S2r, S1rt, S2rt)>=f2(S1r, S2r)?true:false));
+//						System.out.println();
+						F2.add(f2(S1r, S2r));               //F2 score
+						F2_Up.add(M(S1r, S2r, S1rt, S2rt)); //F2_upper bound
+						F.add((f1(S1r, S2r)- f2(S1r, S2r) + penalty(S1r)));                //F score
+						F_Low.add((f1(S1r, S2r)- M(S1r, S2r, S1rt, S2rt) + penalty(S1r))); //F_lower bound
+					}
+				}
+			}
+//			System.out.println("S1 size: " + S1r.size() + "\tS2 size: "+S2r.size());
+//			System.out.println("end "+r+" lower bound score: "+ score);
+			System.out.println();
+		} while (!(compare(S1r, S1rt) && compare(S2r, S2rt)));
+		System.out.println("Complete!!");
+		// when converge, print the result user set to console
+		int i;
+		for (i=0; i<S1r.size(); i++) {
+			System.out.print(userLocMap.get(S1r.get(i)));
+			if(i%5==4)
+				System.out.println();
+			else{
+				if (userLocMap.get(S1r.get(i)).length()<8)
+					System.out.print("\t");
+				System.out.print("\t");
+			}
+		}
+		if(i%5!=0)
+			System.out.println();
+	}
+	
+	// shuffle the unchosen element to take the next iteration
+	private static void randomPermutation() {
+		random.clear();
+		random.addAll(V1);random.removeAll(S1rt);
+		random.addAll(V2);random.removeAll(S2rt);
+		Collections.shuffle(random);
+		System.out.println("random: " + random.size());
+	}
+	
+	// compare two ArrayList are equal or not
+	private static <T extends Comparable<T>> boolean compare(ArrayList<T> a, ArrayList<T> b) {
+	    if(a.size() != b.size())
+	        return false;
+	    Collections.sort(a);
+	    Collections.sort(b);
+	    for(int i=0;i<a.size();i++){
+	        if(!a.get(i).equals(b.get(i)))
+	            return false;
+	    }
+	    return true;
+	}
+	
+	// F = F1 - MF2 + penalty
+	private static double f(ArrayList<String> S1, ArrayList<String> S2, ArrayList<String> S1rt, ArrayList<String> S2rt) {
+		double result = 0;
+		result += f1(S1, S2);
+		result -= M(S1, S2, S1rt, S2rt);
+		result += penalty(S1);
+		return result;
+	}
+	
+	// F1 = -(|S1|+|S2|)log(|S1|+|S2|)
+	private static double f1(ArrayList<String> S1, ArrayList<String> S2) {
+		if (S1.size() + S2.size() != 0)
+			return -(S1.size() + S2.size()) * Math.log(S1.size() + S2.size());
+		else 
+			return 0;
+	}
+	
+	// MF2 = GY(S)+F2(Y)-GY(Y)
+	private static double M(ArrayList<String> S1, ArrayList<String> S2, ArrayList<String> S1rt, ArrayList<String> S2rt) {
+		double x = 0;
+		int method = 2;
+		switch(method) {
+		case 0: x = G0(S1, S2, S1rt, S2rt) + FY - GYY; break;
+		case 1: x = FY - G1(S1, S2, S1rt, S2rt); break;
+		case 2: x = FY + G2(S1, S2, S1rt, S2rt); break;
+		}
+		
+		return x;
+	}
+	
+	private static double G0(ArrayList<String> S1, ArrayList<String> S2, ArrayList<String> S1rt, ArrayList<String> S2rt) {
+		double x = 0;
+		ArrayList<String> t = new ArrayList<String>();
+		for (int i=0; i<S1.size(); i++) {
+			String e = S1.get(i);
+			if (S1rt.contains(e)) {
+				x += FV;
+				t.clear();t.addAll(V1);t.remove(e);
+				x -= f2(t, V2);
+			}
+			else {
+				t.clear();t.addAll(S1rt);t.add(e);
+				x += f2(t, S2rt);
+				x -= FY;
+			}
+		}
+		for (int i=0; i<S2.size(); i++) {
+			String e = S2.get(i);
+			if (S2rt.contains(e)) {
+				x += FV;
+				t.clear();t.addAll(V2);t.remove(e);
+				x -= f2(V1, t);
+			}
+			else {
+				t.clear();t.addAll(S2rt);t.add(e);
+				x += f2(S1rt, t);
+				x -= FY;
+			}
+		}
+		return x;
+	}
+	private static double G1(ArrayList<String> S1, ArrayList<String> S2, ArrayList<String> S1rt, ArrayList<String> S2rt) {
+		double x = 0;
+		ArrayList<String> t = new ArrayList<String>();
+		ArrayList<String> s = new ArrayList<String>();
+		s.addAll(S1rt);s.addAll(S2rt);s.removeAll(S1);s.removeAll(S2);
+		for (int i=0; i<s.size(); i++) {
+			String e = s.get(i);
+			if (isUser(e)) {
+				x += FY;
+				t.clear();t.addAll(S1rt);t.remove(e);
+				x -= f2(t, S2rt);
+			}
+			else {
+				x += FY;
+				t.clear();t.addAll(S2rt);t.remove(e);
+				x -= f2(S1rt, t);
+			}
+		}
+		s.clear();s.addAll(S1);s.addAll(S2);s.removeAll(S1rt);s.removeAll(S2rt);
+		ArrayList<String> empty = new ArrayList<String>();
+		empty.clear();
+		for (int i=0; i<s.size(); i++) {
+			String e = s.get(i);
+			if (isUser(e)) {
+				t.clear();t.add(e);
+				x += f2(t, empty);
+				x -= f2(empty, empty);
+			}
+			else {
+				t.clear();t.add(e);
+				x += f2(empty, t);
+				x -= f2(empty, empty);
+			}
+		}
+		return x;
+	}
+	private static double G2(ArrayList<String> S1, ArrayList<String> S2, ArrayList<String> S1rt, ArrayList<String> S2rt) {
+		double x = 0;
+		ArrayList<String> t = new ArrayList<String>();
+		ArrayList<String> s = new ArrayList<String>();
+		s.addAll(S1rt);s.addAll(S2rt);s.removeAll(S1);s.removeAll(S2);
+		for (int i=0; i<s.size(); i++) {
+			String e = s.get(i);
+			if (isUser(e)) {
+				x += FV;
+				t.clear();t.addAll(V1);t.remove(e);
+				x -= f2(t, V2);
+			}
+			else {
+				x += FV;
+				t.clear();t.addAll(V2);t.remove(e);
+				x -= f2(V1, t);
+			}
+		}
+		s.clear();s.addAll(S1);s.addAll(S2);s.removeAll(S1rt);s.removeAll(S2rt);
+		for (int i=0; i<s.size(); i++) {
+			String e = s.get(i);
+			if (isUser(e)) {
+				x -= FY;
+				t.clear();t.addAll(S1rt);t.add(e);
+				x += f2(t, S2rt);
+			}
+			else {
+				x -= FY;
+				t.clear();t.addAll(S2rt);t.add(e);
+				x += f2(S1rt, t);
+			}
+		}
+		return x;
+	}	
+	// F2 = g(S1,S2)logα - g(S1,S2)log(g(S1,S2)) + (|S1|+|S2|-g(S1,S2))log⁡(1-α) - (|S1|+|S2|-g(S1,S2))log(|S1|+|S2|-g(S1,S2))
+	private static double f2(ArrayList<String> S1, ArrayList<String> S2) {
+		double x = 0;
+		int g = g(S1, S2);
+		x = (double)g * Math.log(alpha);
+		if (g != 0)
+			x -= (double)g * Math.log((double)g);
+		g = S1.size() + S2.size() - g;
+		x += (double)g * Math.log(1.0-alpha);
+		if (g != 0)
+			x -= (double)g * Math.log((double)g);
+		return x;
+	}
+	
+	// g = sum(I(p(S2)(v)<=α)) + sum(I(p(S1)(d)<=α)), here v for user, d for keyword
+	private static int g(ArrayList<String> S1, ArrayList<String> S2) {
+		int x = 0;
+		for (int i=0; i<S1.size(); i++) {
+			String e = S1.get(i);
+			if (p1(S2, e) <= alpha)
+				x++;
+		}
+		for (int i=0; i<S2.size(); i++) {
+			String e = S2.get(i);
+			if (p2(S1, e) <= alpha)
+				x++;
+		}
+		return x;
+	}
+	
+	private static double p1(ArrayList<String> S2, String e) {
+		double x = 0;
+		int ui, ki;
+		
+		if (S2.size() > 0) {
+			ui = u2i.get(e);
+			for (int i=0; i<S2.size(); i++) {
+				ki = k2i.get(S2.get(i));
+				x += pdv[ki][ui];
+			}
+			x = x/(double)S2.size();
+		}
+		return x;
+	}
+	
+	private static double p2(ArrayList<String> S1, String e) {
+		double x = 0;
+		int ui, ki;
+		
+		if (S1.size() > 0) {
+			ki = k2i.get(e);
+			for (int i=0; i<S1.size(); i++) {
+				ui = u2i.get(S1.get(i));
+				x += pdv[ki][ui];
+			}
+			x = x/(double)S1.size();
+		}
+		return x;
+	}
+	
+	// graph cut
+	private static double penalty(ArrayList<String> S1) {
+		double x = 0;
+		int count = 0;
+		for (int i=0; i<S1.size(); i++) {
+			String user = S1.get(i);
+			String loc = userLocMap.get(user);
+			String[] locs = locMap.get(loc).split("\\|");
+			for (int j=0; j<locs.length; j++) {
+				if (locNumMap.containsKey(locs[j]))
+					count += locNumMap.get(locs[j]);
+			}
+			for (int j=0; j<S1.size(); j++) {
+				String u = S1.get(j);
+				for (int k=0; k<locs.length; k++) {
+					if (userLocMap.get(u).equals(locs[k]) && !u.equals(user)){
+						count = count - 1;
+						break;
+					}
+				}
+			}
+		}
+		x = (double)count/100; // this number is the parameter, we can vary it to find better results
+		
+		return -x;
+	}
+	
+	private static boolean isUser(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Date d0 = new Date(114,9,20);//the day before the beginning day, with format "(year-1900, month-1, date)"
+		Date dn = new Date(114,9,21);//the day after the ending day
+		Date date = new Date(114,9,20);
+		date.setTime(d0.getTime());
+		//link database
+		initDB();
+		//init locMap
+		initLocMap();
+		// do random greedy every day
+		do { 
+			date.setTime(date.getTime()+1000*3600*24);
+			System.out.println("Year: "+date.getYear()+" Month: "+date.getMonth()+" Date: "+date.getDate());
+			System.out.println();
+			randomGreedy(date);
+			DrawPlot.draw(F, F_Low, F2, F2_Up);
+		} while(date.before(dn) && date.after(d0));
+	}
+
+}
